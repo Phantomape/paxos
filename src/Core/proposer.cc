22fcbec640feb7e1b46a91c567c88729046786e4
Xucheng Chen <cxucheng@outlook.com>
2018-03-28 10:42:27 -0700

    adding interfaces

--- src/Core/proposer.cc
+++ src/Core/proposer.cc
@@ -12,12 +12,21 @@ Proposer::~Proposer() {
     std::cout << "Proposer::~Proposer()" << std::endl;
 }
 
-void Proposer::Prepare() {
-    std::cout << "Proposer::Prepare()" << std::endl;
-}
-
 void Propose::Accept() {
     std::cout << "Proposer::Accept()" << std::endl;
 }
 
+void Proposer::Prepare() {
+    std::cout << "Proposer::Prepare()" << std::endl;
+}
+
+void Proposer::ExitPrepare() {}
+void Proposer::ExitAccept() {}
+
+void Proposer::OnAccept() {}
+void Proposer::OnAcceptRejected() {}
+void Proposer::OnAcceptTimeout() {}
+void Proposer::OnPrepare() {}
+void Proposer::OnPrepareRejected() {}
+void Proposer::OnPrepareTimeout() {}
 }
--- src/Core/proposer.h
+++ src/Core/proposer.h
@@ -8,8 +8,19 @@ class Proposer : public Base {
 public:
     Proposer();
     ~Proposer();
-    void Prepare();
+
     void Accept();
+    void Prepare();
+
+    void ExitPrepare();
+    void ExitAccept();
+
+    void OnAccept();
+    void OnAcceptRejected();
+    void OnAcceptTimeout();
+    void OnPrepare();
+    void OnPrepareRejected();
+    void OnPrepareTimeout();
 };
 
 }
