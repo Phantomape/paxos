e0a5cfc63a08d286a6752fd2b25dc03931a50727
Xucheng Chen <cxucheng@outlook.com>
2018-05-02 12:03:13 -0700

    implementation of Queue()

--- include/concurrent.h
+++ include/concurrent.h
@@ -27,24 +27,108 @@ protected:
 template <class T>
 class Queue {
 public:
-    Queue();
-    virtual ~Queue();
+    Queue() : size_(0) {};
+    virtual ~Queue() {};
 
-    virtual size_t add(const T& t, bool signal = true, bool back = true);
-    void broadcast();
-    void clear();
-    bool empty();
-    T& peek();
-    bool peek(T& t, int timeout_ms);
-    size_t pop();
-    size_t pop(T* vals, size_t n);
-    virtual void lock();
-    virtual void unlock();
-    void signal();
-    size_t size() const;
+    virtual size_t add(const T& val, bool signal = true, bool append_to_back = true) {
+        if (append_to_back) {
+            storage_.push_back(val);
+        }
+        else {
+            storage_.push_front(val);
+        }
+
+        if (signal) {
+            cond_.notify_one();
+        }
+
+        return ++size_;
+    };
+
+    void broadcast() {
+        cond_.notify_all();
+    };
+
+    void clear() {
+        storage_.clear();
+    };
+
+    bool empty() {
+        return storage_.empty();
+    };
+
+    T& peek() {
+        while (empty()) {
+            cond_.wait(lock_);
+        }
+        return storage_.front();
+    };
+
+    bool peek(T& val, int timeout_ms) {
+         while (empty()) {
+            if (cond_.wait_for(lock_, std::chrono::milliseconds(timeout_ms)) == std::cv_status::timeout) {
+                return false;
+            }
+        }
+        val = storage_.front();
+        return true;
+    };
+
+    size_t peek(T& val) {
+         while (empty()) {
+            cond_.wait(lock_);
+        }
+        val = storage_.front();
+        return size_;
+    }
+
+    size_t pop() {
+        storage_.pop_front();
+        return --size_;
+    };
+
+    size_t pop(T* vals, size_t n) {
+        while (empty()) {
+            cond_.wait(lock_);
+        }
+
+        size_t i = 0;
+        while (!storage_.empty() && i < n) {
+            vals[i] = storage_.front();
+            storage_.pop_front();
+            --size_;
+            ++i;
+        }
+
+        return i;
+    };
+
+    virtual void lock() {
+        lock_.lock();
+    };
+
+    virtual void unlock() {
+        lock_.unlock();
+    };
+
+    void signal() {
+        cond_.notify_one();
+    };
+
+    size_t size() const {
+        return storage_.size();
+    };
+
+    void swap(Queue& q) {
+        storage_.swap(q.storage_);
+        int size = q.size;
+        q.size_ = size_;
+        size_ = size;
+    }
+
 protected:
     std::mutex lock_;
-    std::condition_variable cond_;
+    std::condition_variable_any cond_;
     std::deque<T> storage_;
     size_t size_;
 };
--- src/core/ioloop.cc
+++ src/core/ioloop.cc
@@ -19,4 +19,5 @@ void IoLoop::Run() {
     }    
 }
 
+
 }
