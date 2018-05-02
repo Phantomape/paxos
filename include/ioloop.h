63fcd1ae052945584072c3a3a46bbd7fb092e3b7
Xucheng Chen <cxucheng@outlook.com>
2018-05-01 20:20:19 -0700

    basic declaration of IoLoop

---
+++ include/ioloop.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "concurrent.h"
+#include "instance.h"
+#include <queue>
+
+namespace paxos {
+
+#define RETRY_QUEUE_MAX_LEN 300
+
+class IoLoop : public Thread {
+public:
+    IoLoop();
+    virtual ~IoLoop();
+
+    void OneLoop(const int timeout_ms);
+    void Run();
+    void Stop();
+private:
+    bool is_end_;
+    bool is_start_;
+
+    int queue_size_;
+
+    Instance* instance;
+    Queue<std::string*> message_queue;
+    std::queue<PaxosMsg> retry_queue;
+};
+
+}
