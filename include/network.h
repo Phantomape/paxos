e435a4a876e786fdeabcd4e4491a1d43d7f2d96c
Xucheng Chen <cxucheng@outlook.com>
2018-05-02 18:22:42 -0700

    added declaration for Network

---
+++ include/network.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+namespace paxos {
+
+class Node;
+
+class Network {
+public:
+    Network();
+    virtual ~Network() {}
+
+    //Network must not send/recieve any message before paxoslib called this funtion.
+    virtual void RunNetwork() = 0;
+
+    //If paxoslib call this function, network need to stop receive any message.
+    virtual void StopNetwork() = 0;
+
+    virtual int SendMessageTCP(const int group_idx, const std::string& ip, const int port, const std::string& message) = 0;
+
+    virtual int SendMessageUDP(const int group_idx, const std::string& ip, const int port, const std::string& message) = 0;
+
+    //When receive a message, call this funtion.
+    //This funtion is async, just enqueue an return.
+    int OnReceiveMessage(const char* message, const int message_len);
+
+private:
+    friend class Node;
+    Node* node;
+};
+    
+}
