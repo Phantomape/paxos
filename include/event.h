81c5e97aad3974eb9602b6469e3882e343dec019
Xucheng Chen <cxucheng@outlook.com>
2018-05-03 14:55:17 -0700

    added declaration of Event

---
+++ include/event.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+
+namespace paxos {
+
+class EventLoop;
+
+class Event {
+public: 
+    Event(EventLoop* event_loop);
+    virtual ~Event();
+
+    virtual int GetSocketFd() const = 0;
+
+    virtual const std::string & GetSocketHost() = 0;
+
+    virtual int OnRead();
+
+    virtual int OnWrite();
+
+    virtual void OnError(bool& need_delete) = 0;
+
+    virtual void OnTimeout(const uint32_t timer_id, const int type);
+
+public:
+    void AddEvent(const int event);
+    
+    void RemoveEvent(const int event);
+
+    void JumpoutEpollWait();
+
+    const bool IsDestroy() const;
+
+    void Destroy();
+
+public:
+    void AddTimer(const int timeout_ms, const int type, uint32_t& timer_id);
+
+    void RemoveTimer(const uint32_t timer_id);
+
+protected:
+    int event_;
+    EventLoop* event_loop;
+
+    bool is_destroried_;
+};
+
+}
