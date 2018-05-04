76624b3c3b0c6a649f36d2f01a59fc40384daadf
Xucheng Chen <cxucheng@outlook.com>
2018-05-04 10:31:48 -0700

    added basic declaration of TcpClient

---
+++ include/tcp_client.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "message_event.h"
+#include <map>
+#include <string>
+#include <vector>
+
+namespace paxos {
+
+class EventLoop;
+class Network;
+
+class TcpClient {
+public:
+    TcpClient(EventLoop* eventloop, Network* network);
+    ~TcpClient();
+
+    int AddMessage(const std::string& ip, const int port, const std::string& message);
+
+    void DealWithWrite();
+
+private:
+    MessageEvent* CreateEvent(const uint64_t node_id, const std::string& ip, const int port);
+    MessageEvent* GetEvent(const std::string& ip, const int port);
+    
+    EventLoop* eventloop_;
+    Network* network_;
+
+    std::map<uint64_t, MessageEvent*> map_event_;
+    std::vector<MessageEvent*> vec_event_;
+    std::mutex mutex_;
+};
+
+}
