75df2ffae81c495769d45b6ea133dde0f1995a8b
Xucheng Chen <cxucheng@outlook.com>
2018-05-03 17:17:47 -0700

    added declaration of NotifyEvent

---
+++ include/notify_event.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "event.h"
+#include <string>
+
+namespace paxos {
+
+class EventLoop;
+
+class NotifyEvent : public Event {
+public:
+    NotifyEvent(EventLoop* event_loop);
+    ~NotifyEvent();
+
+    int Init();
+
+    int GetSocketFd() const;
+
+    const std::string & GetSocketHost();
+
+    void SendNotifyEvent();
+
+    int OnRead();
+
+    void OnError(bool & bNeedDelete);
+
+private:
+    int pipe_fd[2];
+    std::string host;
+};
+
+}
