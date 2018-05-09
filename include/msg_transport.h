e5a07967d7ed213c8a1c9981a51f1c1598c9133c
Xucheng Chen <cxucheng@outlook.com>
2018-05-09 11:28:22 -0700

    added declaration of MsgTransport

---
+++ include/msg_transport.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "options.h"
+
+namespace paxos {
+
+enum Message_SendType {
+    Message_SendType_UDP = 0,
+    Message_SendType_TCP = 1,
+};
+
+class MsgTransport {
+public:
+    virtual ~MsgTransport() {}
+
+    virtual int SendMessage(
+        const int group_idx, 
+        const uint64_t recv_node_id, 
+        const std::string & buffer, 
+        const int send_type = Message_SendType_UDP
+    ) = 0;
+
+    virtual int BroadcastMessage(
+        const int group_idx, 
+        const std::string & buffer, 
+        const int send_type = Message_SendType_UDP
+    ) = 0;
+    
+    virtual int BroadcastMessageFollower(
+        const int group_idx, 
+        const std::string & buffer, 
+        const int send_type = Message_SendType_UDP
+    ) = 0;
+    
+    virtual int BroadcastMessageTempNode(
+        const int group_idx, 
+        const std::string & buffer, 
+        const int send_type = Message_SendType_UDP
+    ) = 0;
+};
+    
+}
