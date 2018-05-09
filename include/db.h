bc65c00d695342dfa5527d3c8a9171fe1a3c3577
Xucheng Chen <cxucheng@outlook.com>
2018-05-08 18:08:12 -0700

    updated declaration of Database

--- include/db.h
+++ include/db.h
@@ -1,28 +1,9 @@
-/*
-Tencent is pleased to support the open source community by making 
-PhxPaxos available.
-Copyright (C) 2016 THL A29 Limited, a Tencent company. 
-All rights reserved.
-
-Licensed under the BSD 3-Clause License (the "License"); you may 
-not use this file except in compliance with the License. You may 
-obtain a copy of the License at
-
-https://opensource.org/licenses/BSD-3-Clause
-
-Unless required by applicable law or agreed to in writing, software 
-distributed under the License is distributed on an "AS IS" basis, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
-implied. See the License for the specific language governing 
-permissions and limitations under the License.
-
-See the AUTHORS file for names of contributors. 
-*/
-
 #pragma once
 
 #include "leveldb/db.h"
 #include "leveldb/comparator.h"
+#include "log_storage.h"
+#include "log_store.h"
 #include <vector>
 #include <string>
 #include <map>
@@ -114,10 +95,7 @@ private:
     TimeStat m_oTimeStat;
 };
 
-//////////////////////////////////////////
-
-class MultiDatabase : public LogStorage
-{
+class MultiDatabase : public LogStorage {
 public:
     MultiDatabase();
     ~MultiDatabase();
