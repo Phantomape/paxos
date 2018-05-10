8d8323476a6bf40ca06575d679fd5691add954e6
Xucheng Chen <cxucheng@outlook.com>
2018-05-10 13:59:28 -0700

    added declaration of SerialLock

---
+++ include/serial_lock.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+namespace paxos {
+
+class SerialLock {
+public:
+    SerialLock();
+
+    ~SerialLock();
+
+    void Lock();
+
+    void UnLock();
+
+    void Wait();
+    
+    void Interupt();
+
+    bool WaitTime(const int iTimeMs);
+
+private:
+    std::mutex m_oMutex;
+    std::condition_variable_any m_oCond;
+};
+
+}
