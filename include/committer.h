0afd80439815ddb3ddb734a4ecc4ec3f63f072e7
Xucheng Chen <cxucheng@outlook.com>
2018-05-10 14:44:37 -0700

    added declaration of Committer

---
+++ include/committer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "config.h"
+#include "state_machine_base.h"
+#include "wait_lock.h"
+#include <string>
+#include <inttypes.h>
+
+namespace paxos
+{
+
+class CommitCtx;
+class IoLoop;
+
+class Committer
+{
+public:
+    Committer(Config * poConfig, CommitCtx * poCommitCtx, IoLoop * poIOLoop, StateMachineFac * poStateMachineFac);
+    ~Committer();
+
+public:
+    int NewValueGetID(const std::string & sValue, uint64_t & llInstanceID);
+    
+    int NewValueGetID(const std::string & sValue, uint64_t & llInstanceID, StateMachineCtx * poStateMachineCtx);
+    
+    int NewValueGetIDNoRetry(const std::string & sValue, uint64_t & llInstanceID, StateMachineCtx * poStateMachineCtx);
+
+    int NewValue(const std::string & sValue);
+
+public:
+    void SetTimeoutMs(const int iTimeoutMs);
+
+    void SetMaxHoldThreads(const int iMaxHoldThreads);
+
+    void SetProposeWaitTimeThresholdMS(const int iWaitTimeThresholdMS);
+
+private:
+    void LogStatus();
+
+private:
+    Config * m_poConfig;
+    CommitCtx * m_poCommitCtx;
+    IoLoop * m_poIOLoop;
+    StateMachineFac * m_poSMFac;
+
+    WaitLock m_oWaitLock;
+    int m_iTimeoutMs;
+
+    uint64_t m_llLastLogTime;
+};
+    
+}
