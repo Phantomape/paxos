08a1cba53a4bbe532d01d53d9329e31c21078bf2
Xucheng Chen <cxucheng@outlook.com>
2018-05-10 14:45:19 -0700

    added declaration of CommitCtx

---
+++ include/commit_ctx.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "config.h"
+#include "serial_lock.h"
+#include "state_machine.h"
+#include <string>
+
+namespace paxos {
+
+class StateMachine;
+
+class CommitCtx
+{
+public:
+    CommitCtx(Config * poConfig);
+    ~CommitCtx();
+
+    void NewCommit(std::string * psValue, StateMachineCtx * poSMCtx, const int iTimeoutMs);
+    
+    const bool IsNewCommit() const;
+
+    std::string & GetCommitValue();
+
+    void StartCommit(const uint64_t llInstanceID);
+
+    bool IsMyCommit(const uint64_t llInstanceID, const std::string & sLearnValue, StateMachineCtx *& poSMCtx);
+
+public:
+    void SetResult(const int iCommitRet, const uint64_t llInstanceID, const std::string & sLearnValue);
+
+    void SetResultOnlyRet(const int iCommitRet);
+
+    int GetResult(uint64_t & llSuccInstanceID);
+
+public:
+    const int GetTimeoutMs() const;
+
+private:
+    Config * m_poConfig;
+
+    uint64_t m_llInstanceID;
+    int m_iCommitRet;
+    bool m_bIsCommitEnd;
+    int m_iTimeoutMs;
+
+    std::string * m_psValue;
+    StateMachineCtx * m_poSMCtx;
+    SerialLock m_oSerialLock;
+};
+}
