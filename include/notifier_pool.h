1589bb4fdf728f277c9c03873bc29226229042ca
Xucheng Chen <cxucheng@outlook.com>
2018-05-10 11:45:43 -0700

    added declaration of NotifierPool

---
+++ include/notifier_pool.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <map>
+#include <mutex>
+
+namespace paxos {
+
+class Notifier
+{
+public:
+    Notifier();
+
+    ~Notifier();
+
+    int Init();
+
+    void SendNotify(const int ret);
+
+    void WaitNotify(int & ret);
+
+private:
+    int m_iPipeFD[2];
+};
+
+class NotifierPool
+{
+public:
+    NotifierPool();
+
+    ~NotifierPool();
+
+    int GetNotifier(const uint64_t iID, Notifier *& poNotifier);
+
+private:
+    std::map<uint64_t, Notifier *> m_mapPool;
+    std::mutex m_oMutex;
+};
+
+}
