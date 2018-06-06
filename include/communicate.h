b7f68d4c768bcc8a11526968dcd9de05a2597115
Xucheng Chen <cxucheng@outlook.com>
2018-06-06 16:03:38 -0700

    Remove additional empty space in Communicate

--- include/communicate.h
+++ include/communicate.h
@@ -14,27 +14,27 @@ public:
     virtual ~Communicate() {}
 
     virtual int SendMessage(
-        const int group_idx, 
-        const uint64_t recv_node_id, 
-        const std::string & buffer, 
+        const int group_idx,
+        const uint64_t recv_node_id,
+        const std::string & buffer,
         const int send_type = Message_SendType_UDP
     ) = 0;
 
     virtual int BroadcastMessage(
-        const int group_idx, 
-        const std::string & buffer, 
+        const int group_idx,
+        const std::string & buffer,
         const int send_type = Message_SendType_UDP
     ) = 0;
     
     virtual int BroadcastMessageFollower(
-        const int group_idx, 
-        const std::string & buffer, 
+        const int group_idx,
+        const std::string & buffer,
         const int send_type = Message_SendType_UDP
     ) = 0;
     
     virtual int BroadcastMessageTempNode(
-        const int group_idx, 
-        const std::string & buffer, 
+        const int group_idx,
+        const std::string & buffer,
         const int send_type = Message_SendType_UDP
     ) = 0;
 };
